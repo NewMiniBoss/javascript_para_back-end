98ec57c05ca05c7f662be9262da3bc6d1197a21c
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-12-01 13:47:23 -0300

    add script.js and tutorial null and undefinid

---
+++ null_and_undefined.js
@@ -0,0 +1,7 @@
+// null = ausência de valor
+let input = null;
+console.log(input); // null
+
+// undefined = valor não definido
+let input2;
+console.log(input2); // undefined
---
+++ script.js
@@ -0,0 +1,34 @@
+// var
+// var altura = 5;
+// var comprimento = 10;
+// var area;
+// area = altura + comprimento
+// console.log(area); 
+
+// let
+// let forma = 'retangulo';
+// let altura = 5;
+// let comprimento = 10;
+// let area;
+
+// if (forma === 'retangulo') {
+//     area = altura + comprimento;
+// } else {
+//     area = (altura + comprimento) / 2;
+// }
+
+// console.log(area);
+
+// const
+const forma = 'retangulo';
+const altura = 5;
+const comprimento = 10;
+let area;
+
+if (forma === 'retangulo') {
+    area = altura + comprimento;
+} else {
+    area = (altura + comprimento) / 2;
+}
+
+console.log(area);
