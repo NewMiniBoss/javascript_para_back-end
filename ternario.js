c66acb1ec2bebac4d9f64f29c89ffa1e179a0f4d
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-12-01 17:47:43 -0300

    ternario.js console.log(operadores)

---
+++ ternario.js
@@ -0,0 +1,12 @@
+const idadeMinima = 18
+const idadeCliente = 17
+
+if (idadeCliente >= idadeMinima) {
+    console.log('cerveja')
+} else {
+    console.log('suco')
+}
+
+// operador ternário
+            //condição                     //if true     //else false
+console.log(idadeCliente >= idadeMinima ? 'cerveja' : 'suco')
