a227a875f33441ee63885eda0c0431aa0a0cc48e
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-12-01 18:52:50 -0300

    funcoes e return update

--- funcoes.js
+++ funcoes.js
@@ -8,11 +8,21 @@ function soma() {
     return 2 + 2    // return tem que ser a ultima linha
 }
 
-// 3) executa função
+// 3) executa função// 3) função com return
+function soma() {
+    return 2 + 2
+}
+
+// 4) executando função
 imprimeTexto('primeiro teste')
 imprimeTexto('segundo teste')
 
-// 4) executando função com return
+// 4.1) executando função com return
 imprimeTexto(soma());
 
 
+function retornado() {
+    return console.log('olá return')
+}
+
+retornado()
