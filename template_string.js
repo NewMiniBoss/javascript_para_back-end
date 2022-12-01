88f90cddf3ad6afcc32a551f68469ecd4d0f46df
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-12-01 18:04:00 -0300

    template string, melhor forma de concatenar

---
+++ template_string.js
@@ -0,0 +1,7 @@
+const nome = 'Gabriel'
+const idade = 2022 - 2000
+const cidade = 'São Paulo'
+
+// template string
+const apresentacao = `meu nome é ${nome} minha idade é ${idade}, e nasci na cidade de ${cidade}`
+console.log(apresentacao)
