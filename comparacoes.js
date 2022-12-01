3b2b653d26fa2450c5639a4cd0c712d4ede492c7
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-12-01 16:37:35 -0300

    comparacoes implicítas e explícitas e suas conversões

---
+++ comparacoes.js
@@ -0,0 +1,12 @@
+const numero = 5
+const numeroTexto = '5'
+
+// == (comparação implícita)
+console.log(numero == numeroTexto) // compara o valor
+
+// === (comparação explícita)
+console.log(numero === numeroTexto) // compara o valor e o tipo de dado
+
+// usar conversões
+Number() 
+String()
