b936cef308779aa04802671637d6c93042bcac9c
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-12-01 14:30:25 -0300

    add conversions js, tutorial

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// tipo de dado
+
+// booleanos
+
+// conversão implícita
+const numero = 465
+const numeroString = '465'
+
+console.log(numeroString + numero)
+console.log(Number(numeroString) + numero) // conversão da string Number()
+
+// console.log(numero == numeroString) // comparacação pelo valor
+// console.log(numero === numeroString) // comparação pelo atributo e valor
+
+// conversão explícita
