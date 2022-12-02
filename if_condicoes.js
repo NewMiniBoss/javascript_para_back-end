87b0d91aff94757debd8bb764a16b5bd3364d18f
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-12-01 22:25:25 -0300

    condicoes if

---
+++ if_condicoes.js
@@ -0,0 +1,10 @@
+// OR = || = OU
+const num = 11;
+if (num > 10 || !num) {
+ console.log('número não válido');
+} 
+// AND = && = E
+const num2 = 11;
+if (num2 > 10 && num2 < 20) {
+ console.log('número válido');
+}
