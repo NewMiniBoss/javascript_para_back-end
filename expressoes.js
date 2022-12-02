957237fc6bd9112e89d54f290bb5971ed598ea73
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-12-01 21:56:56 -0300

    expressoes das funcoes

---
+++ expressoes.js
@@ -0,0 +1,9 @@
+// 1) function com nome
+function funcaoComNome() { return 'função com nome' }
+console.log(funcaoComNome())
+// 2) function sem nome
+const funcaoSemNome = function () { return 'função sem nome' }
+console.log(funcaoSemNome())
+// 3) function anônima
+const funcaoAnonima = () => { return 'função anônima' }
+console.log(funcaoAnonima())
