4943d04b4d8514a52ff3cf519700a64cfe029676
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-12-07 14:05:08 -0300

    concat (unindo arrays)

---
+++ Arrays/juntando-salas.js
@@ -0,0 +1,5 @@
+const salaJs = ['lazari', 'gatux', 'chugy']
+const salaPython = ['moreno', 'marilu', 'paizao']
+// unificando (concatenado)
+const salaUnificada = salaJs.concat(salaPython)
+console.log(salaUnificada);
