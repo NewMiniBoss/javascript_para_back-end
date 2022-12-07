b4812dba99ff1def7ca2fc48bec05610a130eae8
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-12-07 15:04:48 -0300

    matrizes em arrays

--- Arrays/duas-dimensoes.js
+++ Arrays/duas-dimensoes.js
@@ -4,3 +4,10 @@ const alunosENotas = [alunos, notas]
 console.log(alunosENotas)
 // puxando em lista arrays da arrays
 console.log(`O aluno ${alunosENotas[0][3]} tirou nota ${alunosENotas[1][3]} na prova`)
+
+// matrizes em arrays
+const funcionarios = [
+    ["Ana", "Juliana", "Leonardo"],
+    [30, 35, 28]
+];
+console.log(`${funcionarios[0][1]} tem ${funcionarios[1][2]} anos`)
