f6a10404ee15600aadd0ae633361cbec2a079e39
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-12-07 13:19:44 -0300

    splice (adicionando e removendo itens das listas arrays

---
+++ Arrays/atualizando-lista.js
@@ -0,0 +1,8 @@
+const nomes = ["João", "Ana", "Caio"]
+// removendo arrays
+nomes.splice(0, 1)
+// add alguém
+nomes.splice(0, 0, 'davi')
+nomes.splice(0, 0, 'Gabriel', 'bia')
+        //lugar onde vai tirar //quantos vai tirar // adicionar alguém
+console.log(nomes)
